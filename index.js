1a57b3236ff50c1acbc6f027f4a1db8727380f50
Sivaraj Nagaraj <sivaraj.nagaraj@BRILLIO.COM>
2017-06-14 17:06:12 +0530

    test json serve

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var test = require('./public/json/test');
+var categoryList = require('./public/json/categoryList');
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
+app.get('/json/test', function (req, res) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.write(JSON.stringify(test));
+  res.end();
+})
+
+app.get('/json/categoryList', function (req, res) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.write(JSON.stringify(categoryList));
+  res.end();
+})
+
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
